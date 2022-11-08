af8b09b62099a1889868a8473dbc4f99bdc91916
Mishkadong <mishkadong@gmail.com>
2022-11-09 00:40:29 +0100

    add RatingStars component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,15 @@
 import "./Footer.scss";
 import SocialMediaList from "../SocialMediaList/SocialMediaList.js";
+import RatingStarClass from "../RatingStars/RatingStarsClass";
+import RatingStars from "../RatingStars/RatingStars";
 
 function Footer() {
   return (
     <footer className="footer">
 
       <SocialMediaList />
+      <RatingStars />
+      <RatingStarClass />
       <span className="footer__copyright">copyright 2022</span>
       <span className="footer__date">
         {Date()}
